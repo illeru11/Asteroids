cd83080b92742946d5875b184749e988eb5fb090
illeru11 <119720728+illeru11@users.noreply.github.com>
2025-07-14 18:03:36 +1000

    switch to win system and clock tick

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from constants import *
 def main():
     
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     #print("Starting Asteroids!")
     #print(f"Screen width: {SCREEN_WIDTH}")
@@ -15,7 +18,14 @@ def main():
         
         screen.fill((0, 0, 0))  # Fill the screen with black
         pygame.display.flip()
-
+        
+        
+        
+        dt_ms = clock.tick(60)
+        
+        dt = dt_ms/1000
+        
+        
 
 
 if __name__ == "__main__":
