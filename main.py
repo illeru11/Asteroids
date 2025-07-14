6965143e6cae9de484a666ccfaaac68a58ebb52d
illeru11 <119720728+illeru11@users.noreply.github.com>
2025-07-14 17:25:52 +1000

    switch to win system

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def main():
     #print(f"Screen height: {SCREEN_HEIGHT}")
 
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
         screen.fill((0, 0, 0))  # Fill the screen with black
         pygame.display.flip()
 
